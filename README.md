72a986c87db6e2e01ebbb6c546676a41ecb3768e
neldivad <dl.eeee.nv@gmail.com>
2024-03-01 21:39:45 +0800

    sasd

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Resources
-- [Privacy policy](/climate-risk-app/resources/mds/privacy_policy.html/)
-- [Terms and condition](/climate-risk-app/resources/mds/terms.html/)
+- [Privacy policy](/climate-risk-app/resources/mds/privacy_policy.html)
+- [Terms and condition](/climate-risk-app/resources/mds/terms.html)
 
--- main.py
+++ main.py
@@ -31,8 +31,8 @@ sidebar_md = """
 ## Resources
 - [Account verification](https://geckointel.com/contact-us)
 - [Submit a bug report](https://geckointel.com/contact-us)
-- [Privacy policy](https://neldivad.github.io/climate-risk-app/resources/mds/privacy_policy.html/)
-- [Terms and condition](https://neldivad.github.io/climate-risk-app/resources/mds/terms.html/)
+- [Privacy policy](https://neldivad.github.io/climate-risk-app/resources/mds/privacy_policy.html)
+- [Terms and condition](https://neldivad.github.io/climate-risk-app/resources/mds/terms.html)
 
 """ #
 
