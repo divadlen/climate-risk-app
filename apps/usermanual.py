9f14d1dccae82434394bb6fc459d00528770317f
David L.E <dl.eeee.nv@gmail.com>
2024-06-09 21:43:23 +0800

    df

--- apps/usermanual.py
+++ apps/usermanual.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 def main():
-
+    # um not updating
     st.markdown("""
     <embed src="https://storage.cloud.google.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf" width="100%" height="1000">
     """, unsafe_allow_html=True)
