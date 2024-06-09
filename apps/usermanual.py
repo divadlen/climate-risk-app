c0032aa9d83aad28a3faeafd217675ae0e0f4b60
David L.E <dl.eeee.nv@gmail.com>
2024-06-09 21:40:38 +0800

    host UM from gcs public

--- apps/usermanual.py
+++ apps/usermanual.py
@@ -1,20 +1,11 @@
 import streamlit as st
-import base64
 
 
 
 def main():
-    # # Path to your PDF file
-    # file_path = 'resources/pdfs/TRACE User Manual v1.2.pdf'
-
-    # # Convert your PDF to a base64 string
-    # with open(file_path, "rb") as pdf_file:
-    #     base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
-
-    # st.markdown(f"""
-    # <embed src="data:application/pdf;base64,{base64_pdf} width="100%" height="900">
-    # """, unsafe_allow_html=True)
 
     st.markdown("""
-    <embed src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="100%" height="900">
+    <embed src="https://storage.cloud.google.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf" width="100%" height="1000">
     """, unsafe_allow_html=True)
+
+    
