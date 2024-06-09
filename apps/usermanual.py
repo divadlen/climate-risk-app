692643803dc3992b686f8aaffa256533044748a1
David L.E <dl.eeee.nv@gmail.com>
2024-06-09 22:08:13 +0800

    fk embeds

--- apps/usermanual.py
+++ apps/usermanual.py
@@ -3,11 +3,13 @@ import streamlit as st
 
 
 def main():
+    st.info('User manual not displaying for you? Try a different browser or go to [this link](https://storage.googleapis.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf)')
+    
     st.markdown("""
     <embed src="https://storage.googleapis.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf" width="100%" height="1000">
     """, unsafe_allow_html=True)
 
-    st.info('User manual not displaying for you? Try a different browser or go to [this link](https://storage.googleapis.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf)')
+    
 
 
     
