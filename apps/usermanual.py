3ef97458ae184168a75deb415576d66e04d9053f
David L.E <dl.eeee.nv@gmail.com>
2024-06-09 21:55:02 +0800

    wtf

--- apps/usermanual.py
+++ apps/usermanual.py
@@ -5,6 +5,8 @@ import streamlit as st
 def main():
     # um not updating
 
+    st.markdown("Loading user manual...")
+
     try:
         st.markdown("""
         <embed src="https://storage.googleapis.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf" width="100%" height="1000">
