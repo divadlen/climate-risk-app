c917a66400791345acc3f6380ebdc8359f0bd282
David L.E <dl.eeee.nv@gmail.com>
2024-06-09 21:48:17 +0800

    why tf pdf not rendering in prod?!

--- apps/usermanual.py
+++ apps/usermanual.py
@@ -4,8 +4,12 @@ import streamlit as st
 
 def main():
     # um not updating
-    st.markdown("""
-    <embed src="https://storage.cloud.google.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf" width="100%" height="1000">
-    """, unsafe_allow_html=True)
+
+    try:
+        st.markdown("""
+        <embed src="https://storage.cloud.google.com/gecko-s3-public/trace/TRACE%20User%20Manual%20v1.2.pdf" width="100%" height="1000">
+        """, unsafe_allow_html=True)
+    except Exception as e:
+        st.error(e)
 
     
